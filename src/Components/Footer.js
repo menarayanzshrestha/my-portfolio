ae0c3ff94e063ddff613d0897e205cfbfc8c3f3f
narayan shrestha <benarayanz@gmail.com>
2022-05-28 13:42:15 +0545

    changed dummy details

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,12 +19,11 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li>Created with ♥️</li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; All rights reserved till {new Date().getFullYear()}</li>
            </ul>
 
         </div>
